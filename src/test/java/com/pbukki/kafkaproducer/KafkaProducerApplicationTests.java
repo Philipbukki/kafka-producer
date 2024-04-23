fd66b70970da373690794b1d8a70976aa479d521
pbukki <philipbukkie@gmail.com>
2024-04-23 17:13:57 +0300

    initial commit - producer microservice

---
+++ src/test/java/com/pbukki/kafkaproducer/KafkaProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pbukki.kafkaproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
